1b231d9038e7ebeee254c301dad9172e92c822f0
ssnover <ssnover95@gmail.com>
2021-09-29 12:44:38 -0600

    test(sav_frlgrse): Verify parsing of team from sav file

--- integration_tests/src/sav_frlgrse.rs
+++ integration_tests/src/sav_frlgrse.rs
@@ -1,6 +1,9 @@
 #![cfg(test)]
 
+use std::convert::TryInto;
 use std::path::Path;
+
+use pk3::PK3_SIZE;
 
 fn open_firered_sav<P: AsRef<Path>>(path: &P) -> std::io::Result<Vec<u8>> {
     let save_bin = sav_frlgrse::open_game_save(path)?;
@@ -9,8 +12,17 @@ fn open_firered_sav<P: AsRef<Path>>(path: &P) -> std::io::Result<Vec<u8>> {
 }
 
 #[test]
-fn verify_parsing_party_size() {
+fn verify_parsing_party() {
     let save = open_firered_sav(&"bins/sav/firered_001.sav").unwrap();
     let team = sav_frlgrse::get_team_pokemon(&save);
     assert_eq!(team.len(), 2);
+
+    let team: Vec<_> = team
+        .into_iter()
+        .map(|data| {
+            pk3::Pokemon::from_bytes_with_encryption(&data[..PK3_SIZE].try_into().unwrap()).unwrap()
+        })
+        .collect();
+    assert_eq!(team[0].species, 7);
+    assert_eq!(team[1].species, 16);
 }
